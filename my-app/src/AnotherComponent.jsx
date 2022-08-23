35060210fa7516041af54391932ecee734f6ba04
tigerFish29 <paulwchabz28@gmail.com>
2022-08-23 12:39:59 +0100

    initial commit

---
+++ my-app/src/AnotherComponent.jsx
@@ -0,0 +1,14 @@
+
+
+const AnotherComponent = () => {
+    return (
+        <div>
+            <p>Hello and welcome to the component </p>
+            <p>React is fun </p>
+            <p>Javascript is good for you </p>
+            <p>React </p>
+        </div>
+    )
+}
+
+export default AnotherComponent;
---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import "./AnotherComponent";
+import AnotherComponent from "./AnotherComponent";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+       <h1>Hello and Welcome</h1>
+        <AnotherComponent />
+    </div>
+  );
+}
+
+export default App;

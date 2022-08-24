15018314a6f516d8ed00b06dd31ec50a0c39364b
tigerFish29 <paulwchabz28@gmail.com>
2022-08-25 00:48:11 +0100

    made changes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,11 @@
 import "./AnotherComponent";
 import AnotherComponent from "./AnotherComponent";
 import "./CustomerData";
+import "./Car";
 
 import './App.css';
 import CustomerData from "./CustomerData";
+import Car from "./Car";
 
 function App() {
   return (
@@ -11,7 +13,13 @@ function App() {
        <h1>Hello and Welcome</h1>
          <AnotherComponent />
          <CustomerData />
+         <div>
+            <h2>Cars</h2>
+            <Car />
+         </div>
     </div>
+
+    
   );
 }
 
